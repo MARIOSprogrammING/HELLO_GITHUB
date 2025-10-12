c94988f9be556d934f4f1a551f65788104f102e9
MARIOSprogrammING <marios.papaxenidis.1992@gmail.com>
2025-10-12 22:35:58 +0300

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,25 @@
+import numpy
+
+
+## STRING ARRAY
+my_helloAR = numpy.array(["hello", "a friend", "nice", "YOUR FRIEND!"])
+
+## STRING NUMBERS
+THE_ARRAY = numpy.array([1,2,3,4])
+
+
+for i in range(0,4,1):
+    print(THE_ARRAY[i]);print(my_helloAR[i])
+    print((1,1,1,1))
+    
+while 1:
+    
+    print(":'D")
+    print(":)")
+    print(":O OKAY FRIEND!")
+    if(1==1):
+        break;
+        
+print("\n\n\n")
+last_words = input("FRIEND RIGHT : ");
+print("LAST WORDS : MY FRIEND")
