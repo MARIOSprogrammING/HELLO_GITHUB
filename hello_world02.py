576b7d035b77dda412630261250ae243a67a9473
MARIOS-production. <marios.papaxenidis.1992@gmail.com>
2025-10-26 05:07:52 +0200

    Add files via upload

---
+++ hello_world02.py
@@ -0,0 +1,16 @@
+
+## HELLO PANDAS WORLD
+
+import pandas
+from pandas import get_dummies as DUMpand
+
+
+print("HELLO BEATIFUL")
+answer_me = input("question : ")
+print("OHH okay : ", answer_me)
+
+bin_list_myHELLO = ["e", "h", "l", "l", "o"]
+
+PA_HELL = input("MY HELLO_WORLD : ")
+print(DUMpand(pandas.Series(bin_list_myHELLO)))
+
