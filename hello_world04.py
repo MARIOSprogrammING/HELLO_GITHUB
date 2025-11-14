8ec5f19c5563813c9a232100a176ae751c0c203d
MARIOS-production. <marios.papaxenidis.1992@gmail.com>
2025-11-14 20:02:31 +0200

    Add files via upload

---
+++ hello_world04.py
@@ -0,0 +1,17 @@
+import unicodedata
+
+
+theCHAR0 = unicodedata.name('H')
+print(theCHAR0)
+theCHAR0 = unicodedata.name('E')
+print(theCHAR0)
+theCHAR0 = unicodedata.name('L')
+print(theCHAR0)
+theCHAR0 = unicodedata.name('L')
+print(theCHAR0)
+theCHAR0 = unicodedata.name('O')
+print(theCHAR0)
+
+
+for i in range(0,4,1) : 
+    print("WORLD")
